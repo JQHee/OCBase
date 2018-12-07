46ba961842ea2baf8126935bb1022261d0aea33b
HJQ <v-huangjiaqian@midland.local>
2018-12-07 11:39:59 +0800

    增加缓存功能

---
+++ OCBase/OCBase/QFFramwork/Components/NetWork/Cache/NetworkCache.h
@@ -0,0 +1,55 @@
+//
+//  NetworkCache.h
+//  OCBase
+//
+//  Created by midland on 2018/12/7.
+//  Copyright © 2018年 HJQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NetworkCache : NSObject
+
+
+/**
+ *  设置缓存路径
+ *
+ *  @param name 路径文件夹的名称
+ */
++ (void)pathName:(NSString *)name;
+
+/**
+ *  缓存网络数据
+ *
+ *  @param responseCache 服务器返回的数据
+ *  @param key           缓存数据对应的key值,推荐填入请求的URL
+ */
++ (void)saveResponseCache:(id <NSCoding>)responseCache forKey:(NSString *)key;
+
+/**
+ *  取出缓存的数据
+ *
+ *  @param key 根据存入时候填入的key值来取出对应的数据
+ *
+ *  @return 缓存的数据
+ */
++ (id)getResponseCacheForKey:(NSString *)key;
+
+/**
+ *
+ *  删除缓存
+ *  @param key 要删除缓存的key值
+ */
++ (void)removeResponseCacheForKey:(NSString *)key;
+
+/**
+ *  删除所有的缓存
+ */
++ (void)removeAllResponseCache;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OCBase/OCBase/QFFramwork/Components/NetWork/Cache/NetworkCache.m
@@ -0,0 +1,103 @@
+//
+//  NetworkCache.m
+//  OCBase
+//
+//  Created by midland on 2018/12/7.
+//  Copyright © 2018年 HJQ. All rights reserved.
+//
+
+#import "NetworkCache.h"
+#import <CommonCrypto/CommonDigest.h>
+#import <PINCache.h>
+
+@implementation NetworkCache
+
+static PINCache *pinCache = nil;
+
+// 区分同用户 （用户id）
++ (void)pathName:(NSString *)name {
+    NSString *cachePath = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject;
+    cachePath = [cachePath stringByAppendingPathComponent:@"com.cache.ocbase"];
+    cachePath = [cachePath stringByAppendingPathComponent:name];
+    pinCache = [[PINCache sharedCache] initWithName:@"name" rootPath:cachePath];
+}
+
++ (void)saveResponseCache:(id <NSCoding>)responseCache forKey:(NSString *)key {
+    if (pinCache == nil) {
+        pinCache = [PINCache sharedCache];
+    }
+    [pinCache setObject:responseCache forKey:[self cachedFileNameForKey: key]];
+}
+
++ (id)getResponseCacheForKey:(NSString *)key {
+    if (pinCache == nil) {
+        pinCache = [PINCache sharedCache];
+    }
+    return [pinCache objectForKey:[self cachedFileNameForKey: key]];
+}
+
++ (void)removeResponseCacheForKey:(NSString *)key {
+    if (pinCache == nil) {
+        pinCache = [PINCache sharedCache];
+    }
+    [pinCache removeObjectForKey:[self cachedFileNameForKey: key]];
+}
+
++ (void)removeAllResponseCache {
+    if (pinCache == nil) {
+        pinCache = [PINCache sharedCache];
+    }
+    [pinCache removeAllObjects];
+}
+
++ (NSString *)cachedFileNameForKey:(NSString *)key {
+    const char *str = key.UTF8String;
+    if (str == NULL) {
+        str = "";
+    }
+    unsigned char r[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(str, (CC_LONG)strlen(str), r);
+    NSString *filename = [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%@",
+                          r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8], r[9], r[10],
+                          r[11], r[12], r[13], r[14], r[15], [key.pathExtension isEqualToString:@""] ? @"" : [NSString stringWithFormat:@".%@", key.pathExtension]];
+    return filename;
+}
+
+/*
++ (NSURLSessionTask *)GET:(NSString *)URL
+               parameters:(id)parameters
+                    cache:(BOOL)cache
+            responseCache:(HttpRequestCache)responseCache
+                  success:(HttpRequestSuccess)success
+                  failure:(HttpRequestFailed)failure {
+    NSString *cacheKey = URL;
+    if (parameters) {
+        cacheKey = [URL stringByAppendingString:[self convertJsonStringFromDictionaryOrArray:parameters]];
+    }
+    if (responseCache) {
+        responseCache([PGNetworkCache getResponseCacheForKey:cacheKey]);
+    }
+    AFHTTPSessionManager *manager = [self manager];
+    return [manager GET:URL parameters:parameters progress:^(NSProgress * _Nonnull uploadProgress) {
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        if (cache) {
+            [PGNetworkCache saveResponseCache:responseObject forKey:cacheKey];
+        }
+        if (success) {
+            success(responseObject);
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        failure ? failure(error) : nil;
+    }];
+}
+ */
+
+/*
++ (NSString *)convertJsonStringFromDictionaryOrArray:(id)parameter {
+    NSData *data = [NSJSONSerialization dataWithJSONObject:parameter options:NSJSONWritingPrettyPrinted error:nil];
+    NSString *jsonStr = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    return jsonStr;
+}
+ */
+
+@end
